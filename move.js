b29de3f3b85a518ec2b4472fe3cbb3449b735087
Clementine DEROME <clementine.derome@gmail.com>
2020-03-21 10:28:08 +0100

    add transition color to h2 on index.html and profile.html

--- move.js
+++ move.js
@@ -1,11 +1,26 @@
+// Dark mode
+
 function switchTheme(event) {
     if (event.target.checked) {
         document.documentElement.setAttribute('data-theme', 'dark');
+        alert('Ça pique les yeux hein ?');
     }
     else {
         document.documentElement.setAttribute('data-theme', 'light');
+        alert('Attention, levée de rideau !');
     }    
 }
 
 const toggler = document.querySelector('#toggler');
 toggler.addEventListener('change', switchTheme, false);
+
+// Change color
+
+const changeColor = document.querySelectorAll('h2')
+for (const i = 0; i < changeColor.length; i++) {
+    (function (h2) {
+        window.setInterval(function () {
+        h2.classList.toggle('blue')
+        }, 1000)
+    })(changeColor[i])
+}
