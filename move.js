7dbf0287fea0d0645f3c2b4c1fe1d712ea904fe3
Clementine DEROME <clementine.derome@gmail.com>
2020-03-21 13:17:33 +0100

    Add a little game in the bottom of index.html

--- move.js
+++ move.js
@@ -17,10 +17,42 @@ toggler.addEventListener('change', switchTheme, false);
 // Change color
 
 const changeColor = document.querySelectorAll('h2')
-for (const i = 0; i < changeColor.length; i++) {
+for (let i = 0; i < changeColor.length; i++) {
     (function (h2) {
         window.setInterval(function () {
         h2.classList.toggle('blue')
         }, 1000)
     })(changeColor[i])
 }
+
+// A little game
+
+    let initGame = document.querySelector('.playGame')
+
+initGame.addEventListener('click', function () {
+    var demo = window.confirm("On va jouer à un jeu >:ˆ)")
+    console.log(demo)
+    var devineLe = Math.round(Math.random() * 10)
+    var essais = 3
+    var essai
+    for (var i = 0; i < 3; i++) {
+        if (i == 0) {
+            essai = prompt('Devine quel chiffre j\'ai en head ! Il se situe entre 0 et 10.  Attention, tu n\'as que 3 essais !')
+        } else {
+        essai = prompt('Try again my friend !')
+        }
+        essai = parseInt(essai, 10)
+        if (essai == devineLe) {
+            break
+        } else if (essai > devineLe) {
+            alert('Trop haut !')
+        } else {
+            alert('Trop bas !')
+        }
+    }
+    if (essai == devineLe) {
+        alert('Bien joué BG :ˆ)')
+    } else {
+        alert('Looooooseeeeeer :ˆ(')
+    }
+})
